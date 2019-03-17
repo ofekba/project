3179033be6b347eece3ef2d99f12a41568db6ca1
Ofek Barazani <37291940+ofekba@users.noreply.github.com>
2019-03-17 10:30:03 +0200

    Add files via upload

--- code/code.py
+++ code/code.py
@@ -1,93 +1,173 @@
 import sys
+import math
 import numpy as np
 from matplotlib import pyplot as plt
 
-fp = open("dist.reax","r") 
-text=fp.read() 
 
-text_list = text.split("\n")
-timeStep=0
-timeStep_index=0
-atom_tag=0
-atom_type=0
-neigh_tag=0
-neigh_dist=0
+def dist_graph():
+	fp = open("dists.reax","r") 
+	text=fp.read() 
 
-arr=np.zeros((81, 11, 11))
-listAtoms=np.zeros(11)
-axis_x=[]
+	text_list = text.split("\n")
+	timeStep=0
+	timeStep_index=0
+	atom_tag=0
+	atom_type=0
+	neigh_tag=0
+	neigh_dist=0
+
+	ln=text_list[0].split(" ")
+	if ln[1] == "totalTimesteps":
+		total_timesteps=int(ln[2])
+
+	neverty_fix_check=10
+
+	size=int(total_timesteps/neverty_fix_check) + 1
+
+	arr=np.zeros((size, 117, 117))
+	axis_x=[]
+
+	fourset=[]
+	fourset_timestep=[]
+	for line in text_list:
+		ln=line.split(" ")
+		i=0 #iterator on the line
+		if ln[i]=="#":
+			i+=1
+		if(i>=len(ln)):
+			continue;
+		elif ln[i]=="Timestep":
+			timeStep=int(ln[i+1])
+			timeStep_index=int(timeStep/neverty_fix_check)
+			axis_x.append(timeStep)
+			continue;
+		elif ln[i]=="atom":
+			atom_tag=int(ln[i+1])
+			atom_type=int(ln[i+3])
+			i+=4
+			j=i #iterator for atoms line
+			while j < len(ln)-1:
+				neigh_tag=int(ln[j])
+				j+=1
+				neigh_dist=float(ln[j])
+				j+=1
+				arr[timeStep_index][atom_tag-1][neigh_tag-1]=neigh_dist
+		elif ln[i]=="fourset":
+			i+=7
+			fs_ts=int(ln[i]) #current fourset timestep
+			i+=2
+			fourset.append([int(x) for x in ln[i+1:i+5]])
+			fourset_timestep.append(fs_ts)
+			if ln[i]=="1/2-":
+				i+=5
+				fourset.append([int(x) for x in ln[i+1:i+5]])
+				fourset_timestep.append(fs_ts)
 
 
-#print(text_list)
+	o_list, h_list, n_list, c_list= [],[],[],[]
+	for list in fourset:
+		o_list.append(list[0])
+		h_list.append(list[1])
+		n_list.append(list[2])
+		c_list.append(list[3])
 
-for line in text_list:
-	ln=line.split(" ")
-	i=0 #iterator on the line
-	if ln[i]=="#":
+
+	typedict =	{
+	  "C": 1,
+	  "H": 2,
+	  "O": 3,
+	  "N": 4
+	}
+
+	axis_y=[ [], [], [], [], [], [], [], [],[], [], [], []]
+	for i in range(len(fourset_timestep)):
+		#fourset 1
+		for j in range(timeStep_index+1):
+			index=i*4
+			axis_y[index].append(arr[j][c_list[i]-1][o_list[i]-1]) #C-O dist
+			axis_y[index+1].append(arr[j][o_list[i]-1][h_list[i]-1]) #O-H dist
+			axis_y[index+2].append(arr[j][n_list[i]-1][h_list[i]-1]) #N-H dist
+			axis_y[index+3].append(arr[j][n_list[i]-1][c_list[i]-1]) #N-C dist
+
+
+	for i in range(len(fourset_timestep)):
+		index=i*4
+		plt.plot(axis_x,axis_y[index], label="C("+str(c_list[i])+")-O("+str(o_list[i])+")")
+		plt.plot(axis_x,axis_y[index+1], label="H("+str(h_list[i])+")-O("+str(o_list[i])+")")
+		plt.plot(axis_x,axis_y[index+2], label="H("+str(h_list[i])+")-N("+str(n_list[i])+")")
+		plt.plot(axis_x,axis_y[index+3], label="C("+str(c_list[i])+")-N("+str(n_list[i])+")")
+		plt.legend(loc='upper right')
+		plt.xlabel("timeStep")
+		plt.ylabel("distance")
+		plt.title("Distance Between Atoms As A Function Of Time, Extra potential started at "+str(fourset_timestep[i]))
+		plt.show()
+	#, linestyle='--'
+
+
+def extraE_graph():
+	e_fp = open("energy.reax","r") 
+	text=e_fp.read() 
+	text_list = text.split("\n")
+
+	text_list=text_list[1:]
+	#print(text_list)
+	y=[]
+	for e in text_list:
+		if e == "finish":
+			y.append(0.0)
+		elif e == "start":
+			continue;
+		else:
+			y.append(float(e))
+
+	#x=list(range(len(y)))
+	#axis_x=list(range(1, len(y)))
+	#print(axis_x)
+
+	x= [i for i in range(len(y))]
+	plt.plot(x,y)
+	plt.xlabel("timeStep")
+	plt.ylabel("energy")
+	plt.title("Additional Energy As A Function Of Time")
+	plt.show()
+
+	
+def E_graph():
+	fp = open("log.lammps","r") 
+	text=fp.read() 
+
+	text_list = text.split("\n")
+
+	i=0
+	for line in text_list:
+		ln=line.split(" ")
 		i+=1
-	if(i>=len(ln)):
-		continue;
-	elif ln[i]=="Timestep":
-		timeStep=int(ln[i+1])
-		timeStep_index=int(timeStep/50)
-		axis_x.append(timeStep)
-		continue;
-	elif ln[i]=="atom":
-		atom_tag=int(ln[i+1])
-		atom_type=int(ln[i+3])
-		if(timeStep_index==0):
-			listAtoms[atom_tag-1]=atom_type
-		i+=4
-		j=i #iterator for atoms line
-		while j < len(ln)-1:
-			neigh_tag=int(ln[j])
-			j+=1
-			neigh_dist=float(ln[j])
-			j+=1
-			arr[timeStep_index][atom_tag-1][neigh_tag-1]=neigh_dist
+		if(ln[0]=="Step"): break
 
-print(list(range(11)),"\n", listAtoms)
-print(arr)
+	timeStep_arr=[]
+	potE_arr=[]
+	totalE_arr=[]
+	for line in text_list[i:]:
+		ln=line.split(" ")
+		ln = [x for x in ln if x != ""]
+		if ln[0]=="Loop": break
+		timeStep_arr.append(int(ln[0]))
+		potE_arr.append(float(ln[2]))
+		totalE_arr.append(float(ln[3]))
+	print(timeStep_arr)
 
-typedict =	{
-  "C": 1,
-  "H": 2,
-  "O": 3,
-  "N": 4
-}
+	plt.plot(timeStep_arr,totalE_arr)
+	plt.xlabel("TimeStep")
+	plt.ylabel("Total Energy")
+	plt.title("Total Energy As A Function Of Time")
+	plt.show()
+	plt.plot(timeStep_arr,potE_arr)
+	plt.xlabel("TimeStep")
+	plt.ylabel("Potential Energy")
+	plt.title("Potential Energy As A Function Of Time")
+	plt.show()
 
-axis_y=[ [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [] ]
-for j in range(timeStep_index+1):
-	axis_y[0].append(arr[j][0][1]) #1-2 dist
-	axis_y[1].append(arr[j][0][2]) #1-3 dist
-	axis_y[2].append(arr[j][0][3]) #1-4 dist
-	axis_y[3].append(arr[j][0][4]) #1-5 dist
-	axis_y[4].append(arr[j][0][6]) #1-7 dist
-	axis_y[5].append(arr[j][1][3]) #2-4 dist
-	axis_y[6].append(arr[j][2][4]) #3-5 dist
-	axis_y[7].append(arr[j][2][6]) #3-7 dist
-	axis_y[8].append(arr[j][4][5]) #5-6 dist
-	axis_y[9].append(arr[j][4][6]) #5-7 dist
-	axis_y[10].append(arr[j][4][8]) #5-9 dist
-	axis_y[11].append(arr[j][4][10]) #5-11 dist
-	axis_y[12].append(arr[j][5][6]) #6-7 dist
-	axis_y[13].append(arr[j][5][7]) #6-8 dist
-	axis_y[14].append(arr[j][5][9]) #6-10 dist
-	axis_y[15].append(arr[j][8][10]) #9-11 dist
 	
-	
-plt.plot(axis_x,axis_y[1], label="1-3")
-plt.plot(axis_x,axis_y[3], label="1-5")
-plt.plot(axis_x,axis_y[7], label="3-7")
-plt.plot(axis_x,axis_y[9], label="5-7", linestyle='--')
-
-plt.legend(bbox_to_anchor=(1.05, 1), loc='upper left', borderaxespad=0.)
-plt.xlabel("timeStep")
-plt.ylabel("distance")
-plt.title("Distance Between Atoms As A Function Of Time")
-plt.show()
-
-
 
 
 
