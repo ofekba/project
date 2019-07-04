e95b1984a86e8975a23fc746ef2c4265c8d3f8c7
Student <ofek1b@gmail.com>
2019-07-04 09:27:24 +0300

    print the result at the end

--- code/python code/cal_cross_linking_percent.py
+++ code/python code/cal_cross_linking_percent.py
@@ -17,7 +17,7 @@ def make_graph(axis_x, axis_y, xlabel="x", ylabel="y", title="Graph"):
 """this code calculate the cross linking percent of a simulation at a timestep
 	that the user types. the calculation:
 	cross linking % = number of created N-C bonds / 4* num of DETDA molecoles.""" 
-def cal_cross_linking_percent(time_step_to_cal):
+def cal_cross_linking_percent(time_step_to_cal, axis_x, axis_y):
 	#gets from the user the timestep he wants to check the cross linking %
 	#time_step_to_cal = int (input("Enter timeStep to calculate cross linking % on: ") )
 	count_n_c_bond=0 #the number of N-C bonds created
@@ -36,6 +36,7 @@ def cal_cross_linking_percent(time_step_to_cal):
 		tag_to_type[int(ln[0])]=int(ln[1])
 
 	start_n_c_bond=0 #number of N-C bonds at the start of the run
+	num_of_detda=num_of_atoms/117
 
 	#check timestep 0 at the bonds file the number of N-C bonds at the start of the run
 	for i in range(num_of_atoms):
@@ -55,41 +56,31 @@ def cal_cross_linking_percent(time_step_to_cal):
 		ln=text_list[i].split(" ")
 		ln=[x for x in ln if x]
 		if len(ln)>2:
-			if ln[1]=="Timestep" and int(ln[2])==time_step_to_cal: break
-	if i>=len(text_list)-1:
-		print("ERROR, CANNOT FIND TIMESTEP ",time_step_to_cal)
-		return
-	i+=7
-	#check the number of N-C bonds at the chosen timestep
-	for j in range(num_of_atoms):
-		ln=text_list[i+j].split(" ")
-		ln=[x for x in ln if x]
-		if(len(ln)<7): break
-		_id=int(ln[0])
-		_type=int(ln[1])
-		if _type!=4: continue
-		_num_neigh=int(ln[2])
-		for n in range(_num_neigh):
-			neigh_id=int(ln[3+n])
-			if(tag_to_type[neigh_id]==1): count_n_c_bond+=1
-	#print the results
-	print("\n=============\nRESULTS:")
-	print("num of N-C bond: ",count_n_c_bond)
-	print("num of N-C bond at the start: ",start_n_c_bond)
-	num_of_detda=num_of_atoms/117
-	cross_linking_percent=(count_n_c_bond-start_n_c_bond)/(4*num_of_detda)
-	print("num of DETDA molecules: ",num_of_detda)
-	print("\nRESULT-cross linking percent: ",cross_linking_percent)
-	return cross_linking_percent
+			if ln[1]=="Timestep" and (int(ln[2])%time_step_to_cal)==0:
+				time_step=int(ln[2])
+				i+=7
+				count_n_c_bond=0
+				#check the number of N-C bonds at the chosen timestep
+				for j in range(num_of_atoms):
+					ln=text_list[i+j].split(" ")
+					ln=[x for x in ln if x]
+					if(len(ln)<7): break
+					_id=int(ln[0])
+					_type=int(ln[1])
+					if _type!=4: continue
+					_num_neigh=int(ln[2])
+					for n in range(_num_neigh):
+						neigh_id=int(ln[3+n])
+						if(tag_to_type[neigh_id]==1): count_n_c_bond+=1
+				cross_linking_percent=(count_n_c_bond-start_n_c_bond)/(4*num_of_detda)
+				axis_x.append(time_step)
+				axis_y.append(cross_linking_percent)
+	print(cross_linking_percent)
 	
 
 axis_x=[]
 axis_y=[]
-for i in range(11):
-    x=i*50000
-    axis_x.append(x)
-    y=cal_cross_linking_percent(x)
-    axis_y.append(y)
+cal_cross_linking_percent(50000, axis_x, axis_y)
 make_graph(axis_x, axis_y, "timeStep", "cross linking %", "Graph of cross linking % as a function of time")
 
 
